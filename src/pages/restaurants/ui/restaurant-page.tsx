99d6baf3445ac9c3fd51b1cb95503907c674bbc7
Magomed Mamergov <m.mamergov@mail.ru>
2026-04-07 01:26:56 +0300

    added restaurants page for system owner

--- src/pages/restaurants/ui/restaurant-page.tsx
+++ src/pages/restaurants/ui/restaurant-page.tsx
@@ -1,26 +1,45 @@
-import { AuthPermission, hasPermission, useAuthStore } from '@/entities/auth'
+import {
+  AuthPermission,
+  getRouteFallback,
+  hasPermission,
+  useAuthStore
+} from '@/entities/auth'
+import { UserRole } from '@/shared/api/services/auth/types'
 import { ErrorBoundary } from '@/shared/ui/error-boundary'
 import { ROUTES } from '@/shared/config/routes'
 import PageHeaderWidget from '@/widgets/page-header'
 import RestaurantsListWidget from '@/widgets/restaurants-list'
 import { Button, Divider, Group, Stack, Text } from '@mantine/core'
-import { Link } from 'react-router'
+import { Link, Navigate } from 'react-router'
 
 export function RestaurantPage() {
   const user = useAuthStore((state) => state.user)
+  const canViewRestaurants = hasPermission(
+    user,
+    AuthPermission.VIEW_RESTAURANTS
+  )
   const canCreateRestaurant = hasPermission(
     user,
     AuthPermission.CREATE_RESTAURANT
   )
+  const isSystemOwner = user?.role === UserRole.SYSTEM_OWNER
+
+  if (!canViewRestaurants) {
+    return <Navigate to={getRouteFallback(ROUTES.RESTAURANTS)} replace />
+  }
 
   return (
     <Stack pb={20}>
-      <PageHeaderWidget title="Список ресторанов" />
+      <PageHeaderWidget
+        title={isSystemOwner ? 'Рестораны системы' : 'Список ресторанов'}
+      />
 
       <Stack className="mt-3 px-5 flex flex-col gap">
         <Group justify="space-between" align="end" gap="md">
           <Text maw={640} className="text-moss-700">
-            Здесь отображается список всех ваших ресторанов.
+            {isSystemOwner
+              ? 'Здесь отображаются все рестораны в системе с возможностью быстро перейти к управлению ими.'
+              : 'Здесь отображается список всех ваших ресторанов.'}
           </Text>
 
           {canCreateRestaurant ? (
