4207771d2e00ed2d0bbe1aca8b705c8e8ff72018
Magomed Mamergov <m.mamergov@mail.ru>
2026-04-13 15:54:07 +0300

    fixes

--- src/features/menu/navigation/index.ts
+++ src/features/menu/navigation/index.ts
@@ -1,3 +1,2 @@
 export { useMenuNavigation } from './model/hooks'
-export { menuNavigationQueryConfig } from './model/config'
 export type { MenuNavigationQuery, MenuNavigationState } from './model/types'
